9463dcaa9b36685bde918336d2c46d31b98219b4
Max <138322092+mx-edu@users.noreply.github.com>
2023-07-01 23:28:15 +0100

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -11,12 +11,12 @@ from core.tools import *
 from core.terminal import *
 from core.interface import *
 
-# Attempt to import/install required packages.
-for package in [
-    "pyshark", "hashlib", "pymetasploit3"
+# Attempt to import/install required Python packages.
+for python_package in [
+    "requests"
 ]:
     try:
-        __import__(package)
+        __import__(python_package)
     except ImportError:
-        std_warning(f"Couldn't import {package}! Installing", end="...")
-        subprocess.run(["pip", "install", package])
+        std_warning(f"Couldn't import {python_package}! Installing", end="...")
+        subprocess.run(["pip", "install", python_package])
